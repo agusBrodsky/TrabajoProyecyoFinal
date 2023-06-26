e4db3c757fd3d95aca4e494ee65e347e66b0044d
agusBrodsky <agusbrodsky2006@gmail.com>
2023-06-25 22:50:51 -0300

    Commit, Formulario completo falta BDD, aprendimos a cambiar de pagina en pagina

--- TrabajoProyecyoFinal/src/components/Input.jsx
+++ TrabajoProyecyoFinal/src/components/Input.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const Input = ({ placeholder, pass = false }) => {
-  const [inputValue, setInputValue] = useState('');
+  
+  const [inputValue, setInputValue] = useState(''); // Estado para guardar el valor del input
 
   const ocultarPass = (text) => {
     if (pass) {
-      setInputValue(text.replace(/./g, '*'));
+      setInputValue(text.replace(/./g, '*')); // si pass = true entonces remplaza por * (esto lo utilizo en el login)
     } else {
       setInputValue(text);
     }
@@ -16,14 +17,14 @@ const Input = ({ placeholder, pass = false }) => {
     <View style={styles.inputContainer}>
       <Text style={styles.tamañoLetra}>{placeholder}</Text>
       <TextInput
-        style={styles.input}
-        value={inputValue}
-        onChangeText={ocultarPass}
+        style={styles.input} // Style
+        value={inputValue} // Guarda el valor en el estado
+        onChangeText={ocultarPass} //Llama a la funcion
       />
     </View>
   );
 };
-
+//Clases--Styles
 const styles = StyleSheet.create({
   inputContainer: {
     alignItems: 'center',
--- TrabajoProyecyoFinal/src/screens/Formulario.jsx
+++ TrabajoProyecyoFinal/src/screens/Formulario.jsx
@@ -1,49 +1,50 @@
+//IMPORTADO DESDE REACT
 import React, { useEffect, useState } from 'react';
-import { Text, View, Alert, StyleSheet, ScrollView, TouchableNativeFeedback, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Text, View, StyleSheet, ScrollView, TouchableNativeFeedback, Button } from 'react-native';
+//COMPONENTES
 import NavBar from '../components/NavBar';
-import repositories from '../data/repositories';
+import preguntitas from '../data/preguntas';
 import Input from '../components/Input';
 import axios from 'axios';
 
-const Formulario = () => {
-  const [data,setData] =useState(null);  
-  const [preguntas, setPreguntas] = useState({});
-  const [border, setBorder] = useState(false);
-  //FETCH
-  const [isLoading, setIsLoading] = useState(true);
-  
 
-  const onPressSi = (id,pregunta) => {
-    setPreguntas((prevPreguntas) => ({
+const Formulario = () => {
+  // ESTADOS
+  const navigation = useNavigation();
+  const [segundaPregunta, setSegundaPregunta] = useState({});
+  const [preguntas, setPreguntas] = useState([]);
+  const [pesatañaForm, setPaginaForm] = useState(1); // Creo un estado para cambiar las preguntas del formulario
+  const [tituloForm, setTituloForm] = useState("EVALUACIÓN MOTORA"); // Estado para cambiar el Titulo del formulario!
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/Pregunta') // No esta probado!
+      .then((res) => {
+        const arrayPreguntas = res.data.data;
+        setPreguntas(arrayPreguntas);
+      });
+  }, []); // Este UseEffect solo se utiliza al correr el programa!
+
+  // Función para manejar el botón "Si"
+  const onPressSi = (id, pregunta) => {
+    setSegundaPregunta((prevPreguntas) => ({
       ...prevPreguntas,
       [id]: true,
     }));
-
-    // FUNCION AXIOS
-
-      axios.get('http://localhost:3000/Pregunta')
-      .then(res =>{
-          setIsLoading(false);
-          console.log("chivatoo");
-        })
-        .catch((error) => {
-          console.log("estamos en error!");
-          console.log(error);
-          setIsLoading(false);
-        });
-
   };
 
+  // Función para manejar el botón "No"
   const onPressNo = (id) => {
-    setPreguntas((prevPreguntas) => ({
+    setSegundaPregunta((prevPreguntas) => ({
       ...prevPreguntas,
       [id]: false,
     }));
   };
 
+  // Componente de botón verde "Si"
   const BotonVerde = ({ id, pregunta }) => {
     return (
-      <TouchableNativeFeedback onPress={() => onPressSi(id,pregunta)}>
+      <TouchableNativeFeedback onPress={() => onPressSi(id, pregunta)}>
         <View style={[styles.claseBoton, styles.verdeBoton]}>
           <Text style={styles.botonTexto}>Si</Text>
         </View>
@@ -51,6 +52,7 @@ const Formulario = () => {
     );
   };
 
+  // Componente de botón rojo "No"
   const BotonRojo = ({ id }) => {
     return (
       <TouchableNativeFeedback onPress={() => onPressNo(id)}>
@@ -61,33 +63,50 @@ const Formulario = () => {
     );
   };
 
+  // Función para continuar al siguiente formulario o pasar de pagina
+  const continuarForm = () => {
+    if (pesatañaForm === 1) {
+      setPaginaForm(2); // Actualiza las preguntas para el formulario no motoras
+      setTituloForm("EVALUACIÓN NO MOTORA"); // Cambia el título del formulario
+    } else {
+      navigation.navigate("Home"); // Navega a la página principal
+    }
+  };
+
   return (
     <View style={styles.container}>
       <NavBar textoInicio='Helpark' />
-
-      <Text style={styles.titulo}>EVALUACIÓN MOTORA</Text>
+      <Text style={styles.titulo}>{tituloForm}</Text>
       <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-        {repositories.map((repo) => (
-          <View key={repo.id} style={styles.claseNoSeMeOcurre}>
-            <Text style={styles.clasePregunta}>{repo.pregunta}</Text>
-            <View style={styles.claseBotonesContainer}>
-              <BotonVerde id={repo.id} pregunta={repo.pregunta}/>
-              <BotonRojo id={repo.id} pregunta={repo.pregunta}/>
-            </View>
-            {preguntas[repo.id] && (
-              <View style={styles.inputContainer}>
-              <Input placeholder={repo.preguntaHabilitada}></Input>
+        {preguntitas.map((repo) => {
+          if (repo.idForm === pesatañaForm) {
+            return (
+              <View key={repo.id} style={styles.claseNoSeMeOcurre}>
+                <Text style={styles.clasePregunta}>{repo.pregunta}</Text>
+                <View style={styles.claseBotonesContainer}>
+                  <BotonVerde id={repo.id} pregunta={repo.pregunta} />
+                  <BotonRojo id={repo.id} pregunta={repo.pregunta} />
+                </View>
+                {segundaPregunta[repo.id] && (
+                  <View style={styles.inputContainer}>
+                    <Input placeholder={repo.preguntaHabilitada} />
+                  </View>
+                )}
               </View>
-            )}
-          </View>
-        ))}
+            );
+          } else {
+            return null;
+          }
+        })}
         <View style={styles.continuarContainer}>
-            <Button title="Continuar" onPress={() => {}} />
-          </View>
+          <Button title="Continuar" onPress={() => continuarForm()} />
+        </View>
       </ScrollView>
     </View>
   );
 };
+
+// Estilos del componente Formulario
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -96,20 +115,16 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     paddingVertical: 20,
   },
-  claseBoton: {
-    // ACAAAA
-    flex: 1,
-    marginHorizontal: 5,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 5,
-  },
+
+
+  // Estilos del título del formulario
   titulo: {
     textAlign: 'center',
     marginTop: '5%',
-    fontSize: 35,
+    fontSize: 32,
   },
+
+  // Estilos de la sección de pregunta
   claseNoSeMeOcurre: {
     marginTop: '10%',
     paddingVertical: 20,
@@ -127,6 +142,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 10,
   },
+
+  // Estilos de los botones
+  claseBoton: {
+    flex: 1,
+    marginHorizontal: 5,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
   verdeBoton: {
     backgroundColor: '#36D06A',
   },
@@ -138,16 +163,19 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
-  textoAdicional:{
-    fontSize:20
+
+  // Estilos del texto adicional
+  textoAdicional: {
+    fontSize: 20,
   },
-  inputContainer:{
-    alignItems:'center',
+
+  // Estilos del contenedor del botón de continuar
+  inputContainer: {
+    alignItems: 'center',
   },
   continuarContainer: {
     alignItems: 'center',
     marginVertical: 20,
   },
 });
-
 export default Formulario;
