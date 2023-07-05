1859f8b1b10a9d1c2608beb3f7c80e293f18e72e
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-05 12:14:03 -0300

    cacas

--- TrabajoProyecyoFinal/src/screens/Formulario.jsx
+++ TrabajoProyecyoFinal/src/screens/Formulario.jsx
@@ -1,166 +1,116 @@
 //IMPORTADO DESDE REACT
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Text, View, StyleSheet, ScrollView, TouchableNativeFeedback,touchableopacity, Button } from 'react-native';
-//COMPONENTES
-import NavBar from '../components/NavBar';
-import preguntitas from '../data/preguntas';
+import { Text, View, StyleSheet, ScrollView, Button } from 'react-native';
 import Input from '../components/Input';
 import axios from 'axios';
-import Constants from 'expo-constants';
 
 const Formulario = () => {
-  // ESTADOS
   const navigation = useNavigation();
-  const [segundaPregunta, setSegundaPregunta] = useState({}); // ES UN ARRAY DE PREGUNTAS {  ORDER== 2  }
-  const [preguntas, setPreguntas] = useState([]); // ARRAY CON TODAS LAS PREGUNTAS!!
-  const [pesatañaForm, setPaginaForm] = useState(1); // Creo un estado para cambiar las preguntas del formulario
-  const [tituloForm, setTituloForm] = useState("EVALUACIÓN MOTORA"); // Estado para cambiar el Titulo del formulario!
-  const [respuesta, setRespuestas] = useState([]);
-  const [arrayRespuestas, setArrayRespuestas] = useState([]);
-
-  const handleInputChange = (value) => {
-    setRespuestas((prevRespuestas) => ({
-      ...prevRespuestas,
-      Texto: value,
-    }));
+  const [segundaPregunta, setSegundaPregunta] = useState({});
+  const [preguntas, setPreguntas] = useState([]); // ARRAY DE PREGUNTAS INICIALES
+  const [pesatañaForm, setPaginaForm] = useState(1);
+  const [tituloForm, setTituloForm] = useState("EVALUACIÓN MOTORA");
+  const [arrayRespuestas, setArrayRespuestas] = useState([]); // ARRAY DE RESPUESTAS!!
+  const [valorInput, setInput] = useState("");
+  const handleInputChange = (value) => { // FUNCION QUE GUARDA VALOR DEL INPUT
+    setInput(value); 
   };
 
-
-  useEffect(() => {
-    axios.get('http://localhost:3000/Pregunta') // No esta probado!
+  useEffect(() => { // SE CORRE CUANDO ARRANCA Y GUARDA EN UN ESTADO TODAS LAS PREGUNTAS
+    axios.get('http://localhost:3000/Pregunta') 
       .then((res) => {
         const arrayPreguntas = res.data;
         setPreguntas(arrayPreguntas);
-
       });
-      
-  }, []); // Este UseEffect solo se utiliza al correr el programa!
-  /*useEffect(() => {
-    axios.post('http://localhost:3000/Respuesta', respuesta)
-    .then((res) => {
-      console.log('Respuesta Enviada con exito');
-    })
-    .catch((error) => {
-      console.log("error al agregar a la base de datos")
-    })
-  },[arrayRespuestas]);*/
-  useEffect(() =>{
-    axios.post('http://localhost:3000/Respuesta', arrayRespuestas)
-    .then((res) => {
-      console.log(respuesta);
-      console.log(arrayRespuestas);
-      console.log("Correcto!");
-    })
-    .catch((error) =>{
-      console.log(respuesta);
-      console.log(arrayRespuestas);
-      console.log("error");
-    })
-  },[pesatañaForm]) 
+  }, []);
 
-  // Función para manejar el botón "Si"
-// Función para manejar el botón "Si"
-const onPressSi = (id, pregunta) => {
-  setSegundaPregunta((prevPreguntas) => ({
-    ...prevPreguntas,
-    [id]: true,
-  }));
+  const onPressSi = (id, pregunta) => { 
+    setSegundaPregunta((prevPreguntas) => ({
+      ...prevPreguntas,
+      [id]: true,
+    }));
 
-  setRespuestas({
-    Id: id,
-    TextoPregunta: pregunta,
-    Opcion: null,
-    Texto: null,
-    IdParteCuerpo: null,
-    Orden: pesatañaForm,
-  });
+    const nuevaRespuesta = { // NUEVA PREGUNTA
+      Id: id,
+      TextoPregunta: pregunta,
+      Opcion: null,
+      Texto: valorInput,
+      IdParteCuerpo: null,
+      Orden: pesatañaForm,
+    };
 
-  setArrayRespuestas((prevRespuestas) => [...prevRespuestas, respuesta]);
-};
-
-// Función para manejar el botón "No"
-const onPressNo = (id, pregunta) => {
-  setSegundaPregunta((prevPreguntas) => ({
-    ...prevPreguntas,
-    [id]: false,
-  }));
-
-  setRespuestas({
-    Id: id,
-    TextoPregunta: pregunta,
-    Opcion: null,
-    Texto: null,
-    IdParteCuerpo: null,
-    Orden: pesatañaForm,
-  });
-
-  setArrayRespuestas((prevRespuestas) => [...prevRespuestas, respuesta]);
-};
-  // Componente de botón verde "Si"
-  const BotonVerde = ({ id, pregunta }) => {
-    return (
-      <Button onPress={() => onPressSi(id, pregunta)} title="Si">
-        <View style={[styles.claseBoton, styles.verdeBoton]}>
-          <Text style={styles.botonTexto}>Si</Text>
-        </View>
-      </Button>
-    );
+    setArrayRespuestas((prevRespuestas) => [...prevRespuestas, nuevaRespuesta]);
   };
 
-  // Componente de botón rojo "No"
-  const BotonRojo = ({ id,pregunta }) => {
-    // <TouchableNativeFeedback></TouchableNativeFeedback>  
-    return (
-      <Button onPress={() => onPressNo(id,pregunta)} title="No">
-        <View style={[styles.claseBoton, styles.rojoBoton]}>
-          <Text style={styles.botonTexto}>No</Text>
-        </View>
-      </Button>
-      
-    );
+  const onPressNo = (id, pregunta) => {
+    setSegundaPregunta((prevPreguntas) => ({
+      ...prevPreguntas,
+      [id]: false,
+    }));
+
+    const nuevaRespuesta = { // NUEVA PREGUNTA
+      Id: id,
+      TextoPregunta: pregunta,
+      Opcion: null,
+      Texto: valorInput,
+      IdParteCuerpo: null,
+      Orden: pesatañaForm,
+    };
+
+    setArrayRespuestas((prevRespuestas) => [...prevRespuestas, nuevaRespuesta]);
   };
 
-  // Función para continuar al siguiente formulario o pasar de pagina
   const continuarForm = () => {
-    if (pesatañaForm != 2) {
-      setPaginaForm(2); // Actualiza las preguntas para el formulario no motoras
-      setTituloForm("EVALUACIÓN NO MOTORA"); // Cambia el título del formulario
+
+    console.log(arrayRespuestas);
+    arrayRespuestas.map((respuesta)=>( 
+      
+    console.log(respuesta),
+    axios.post('http://localhost:3000/Respuesta', respuesta)  
+        .then((res) => {
+          console.log('Respuestas enviadas con éxito');
+        })
+        .catch((error) => {
+          console.log('Error al agregar las respuestas a la base de datos', error);
+        })
+    ))
+    
+    if (pesatañaForm !== 2) {
+      setPaginaForm(2);
+      setTituloForm("EVALUACIÓN NO MOTORA");
     } else {
-      navigation.navigate("Home"); // Navega a la página principal
+      // Realizar la inserción en la base de datos aquí
+      navigation.navigate("Home");
     }
   };
 
   return (
     <View style={styles.container}>
-      <NavBar textoInicio='Helpark' />
       <Text style={styles.titulo}>{tituloForm}</Text>
       <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-        {preguntas.map((repo) => {
-            return (
-              <View key={repo.Id} style={styles.claseNoSeMeOcurre}>
-                <Text style={styles.clasePregunta}>{repo.Texto}</Text>
-                <View style={styles.claseBotonesContainer}>
-                  <BotonVerde id={repo.Id} pregunta={repo.Texto} />
-                  <BotonRojo id={repo.Id} pregunta={repo.Texto} />
-                </View>
-                {segundaPregunta[repo.Id] && (  // ACA HABILITO LA SEGUNDA PREGUNTA!! LE AGREGO UN ID ANTERIORMENTE ASI SE PUEDE VER! 
-                  <View style={styles.inputContainer}>
-                    <Input placeholder={repo.preguntaHabilitada} onChange={handleInputChange} />
-                  </View>
-                )}                
+        {preguntas.map((pregunta) => (
+          <View key={pregunta.Id} style={styles.preguntaContainer}>
+            <Text style={styles.clasePregunta}>{pregunta.Texto}</Text>
+            <View style={styles.claseBotonesContainer}>
+              <Button onPress={() => onPressSi(pregunta.Id, pregunta.Texto)} title="Si" />
+              <Button onPress={() => onPressNo(pregunta.Id, pregunta.Texto)} title="No" />
+            </View>
+            {segundaPregunta[pregunta.Id] && (
+              <View style={styles.inputContainer}>
+                <Input placeholder={pregunta.preguntaHabilitada} onChange={handleInputChange} />
               </View>
-            );
-        })}
+            )}
+          </View>
+        ))}
         <View style={styles.continuarContainer}>
-          <Button title="Continuar" onPress={() => continuarForm()} />
+          <Button title="Continuar" onPress={continuarForm} />
         </View>
       </ScrollView>
     </View>
   );
 };
 
-// Estilos del componente Formulario
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -169,23 +119,14 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     paddingVertical: 20,
   },
-
-
-  // Estilos del título del formulario
   titulo: {
     textAlign: 'center',
     marginTop: '5%',
     fontSize: 32,
   },
-
-  // Estilos de la sección de pregunta
-  claseNoSeMeOcurre: {
+  preguntaContainer: {
     marginTop: '10%',
     paddingVertical: 20,
-  },
-  preguntaContainer: {
-    maxHeight: 100,
-    paddingHorizontal: 20,
   },
   clasePregunta: {
     textAlign: 'center',
@@ -196,34 +137,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 10,
   },
-
-  // Estilos de los botones
-  claseBoton: {
-    flex: 1,
-    marginHorizontal: 5,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 5,
-  },
-  verdeBoton: {
-    backgroundColor: '#36D06A',
-  },
-  rojoBoton: {
-    backgroundColor: '#E10A0A',
-  },
-  botonTexto: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-
-  // Estilos del texto adicional
-  textoAdicional: {
-    fontSize: 20,
-  },
-
-  // Estilos del contenedor del botón de continuar
   inputContainer: {
     alignItems: 'center',
   },
@@ -232,4 +145,5 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
 });
+
 export default Formulario;
