458b43b935a36fd3a38a30b2d57e1cee6d577ca7
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-31 12:06:13 -0300

    jajaajajja

--- TrabajoProyecyoFinal/src/screens/Formulario.jsx
+++ TrabajoProyecyoFinal/src/screens/Formulario.jsx
@@ -1,4 +1,3 @@
-//IMPORTADO DESDE REACT
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Text, View, StyleSheet, ScrollView, Button } from 'react-native';
@@ -13,29 +12,16 @@ const Formulario = () => {
   const [tituloForm, setTituloForm] = useState("EVALUACIÓN MOTORA");
   const [arrayRespuestas, setArrayRespuestas] = useState([]); // ARRAY DE RESPUESTAS!!
   const [valorInput, setInput] = useState("");
-  let idViejo;
-  const handleInputChange = (value,id_r) => { // FUNCION QUE GUARDA VALOR DEL INPUT
-    console.log(id_r);
+ // HACER UNA BUSQEUDA LINEAL CON OPCION (IDPREGUNTA) PARA BUSCAR LOS IGUALES Y ELIMINARLOS (CUNADO AGREGAS A NUEVA FUNCION QUE HAGA LA BUSQUEDA LINEAL ANTES DE AGREGAR A ARRAYRESPUESTA)
+  const handleInputChange = (value, id_r) => {
     setInput(value);
-    let pos = -1, i = 0; 
-
-    while (pos === -1 && i < arrayRespuestas.length) { // FIJARSE ESTA MIERDA
-      if (arrayRespuestas[i].id === id_r) {
-        pos = i;
-        //setArrayRespuestas([i].Texto = value);
-        // setArrayRespuestas((prevRespuestas) => ({
-           
-        // }));
-      }
-      else i++;
-    }
-    actualizarTexto(pos, value); // ARREGLAR ESTA MIERDA
+    actualizarTexto(id_r, value);
     console.log(arrayRespuestas);
   };
 
-  function actualizarTexto(id, value) { // NO ANDAAA!!!!
+  function actualizarTexto(id, value) {
     const actualizado = arrayRespuestas.map(resp => {
-      if (resp.id === id) {
+      if (resp.Orden === id) {
         return { ...resp, Texto: value };
       }
       return resp;
@@ -43,90 +29,93 @@ const Formulario = () => {
     setArrayRespuestas(actualizado);
   }
 
-
-  useEffect(() => { // SE CORRE CUANDO ARRANCA Y GUARDA EN UN ESTADO TODAS LAS PREGUNTAS
+  useEffect(() => {
     axios.get('http://localhost:3000/Pregunta') 
       .then((res) => {
         const arrayPreguntas = res.data;
         setPreguntas(arrayPreguntas);
       });
-      
   }, []);
 
-
-    const onPressSi = (id, pregunta) => { 
-      setSegundaPregunta((prevPreguntas) => ({
-        ...prevPreguntas,
-        [id]: true,
+  const onPressSi = (id, pregunta, idForm) => { 
+    setSegundaPregunta((prevPreguntas) => ({
+      ...prevPreguntas,
+      [id]: true,
     }));
-  
-    const nuevaRespuesta = { // NUEVA PREGUNTA
-      Id: id,
+
+    const nuevaRespuesta = {
+      Id: null,
       TextoPregunta: pregunta,
-      Opcion: null,
-      Texto: valorInput,
+      Opcion: 1,
+      Texto: valorInput !== "" ? valorInput : null,
       IdParteCuerpo: null,
-      Orden: pesatañaForm, // esto hay que actualizarlo, es el orden de pregunta != a id!!!
+      IdForm:idForm+1,
+      Orden: id,
     };
+    //verificarNoIguales(nuevaRespuesta);
+    
     setArrayRespuestas((prevRespuestas) => [...prevRespuestas, nuevaRespuesta]);
+    console.log(arrayRespuestas);
+  };
+  const verificarNoIguales = (nuevaRespuesta)=>{
+    setArrayRespuestas((prevRespuestas) => {
+      return prevRespuestas.filter((respuesta) => {
+        // Compare the properties that need to be unique for each response
+        return respuesta.Orden !== nuevaRespuesta.Orden &&
+          respuesta.TextoPregunta !== nuevaRespuesta.TextoPregunta;
+      });
+    })
+    let idRespuestaRepetida = null; 
+      arrayRespuestas.map((objeto) =>{ // objeto es la respuesta en cada ciclo!
+        
+      })
+    //.............................................................
   }
-  
-  const onPressNo = (id, pregunta) => {
+  const onPressNo = (id, pregunta, idForm) => {
+    console.log("onPressNo");
     setSegundaPregunta((prevPreguntas) => ({
       ...prevPreguntas,
       [id]: false,
     }));
-    console.log(id);
-   // if(idViejo != id){
-    const nuevaRespuesta = { // NUEVA PREGUNTA
-      Id: id,
+
+    const nuevaRespuesta = {
+      Id: null,
       TextoPregunta: pregunta,
-      Opcion: null,
+      Opcion: 0,
       Texto: null,
       IdParteCuerpo: null,
-      Orden: pesatañaForm,
+      IdForm:idForm+1,
+      Orden: id,
     };
-   // }
-    //else{
-      console.log("nooo, estas actualizando!!! jajaajaj");
-      let pepe = preguntas.length -1;
-      console.log(pepe);
-      console.log(preguntas[pepe]);
-      let preguntaParaActualizar  = preguntas;
-      setPreguntas((prevPreguntas) => ({
-
-      }))
-    //}
-
+    verificarNoIguales(nuevaRespuesta);
     setArrayRespuestas((prevRespuestas) => [...prevRespuestas, nuevaRespuesta]);
   };
-// --------------------------------
-  const continuarForm = () => {
 
+  const continuarForm = () => {
     console.log(arrayRespuestas);
-    arrayRespuestas.map((respuesta)=>( 
-      
-    console.log(respuesta),
-    axios.post('http://localhost:3000/Respuesta', respuesta)  
+    arrayRespuestas.forEach((respuesta) => {
+      console.log(respuesta);
+      axios.post('http://localhost:3000/Respuesta', respuesta)  
         .then((res) => {
           console.log('Respuestas enviadas con éxito');
         })
         .catch((error) => {
           console.log('Error al agregar las respuestas a la base de datos', error);
-        })
-    ))
-    
+        });
+    });
+
     if (pesatañaForm !== 2) {
       setPaginaForm(2);
       setTituloForm("EVALUACIÓN NO MOTORA");
     } else {
-      // Realizar la inserción en la base de datos aquí
       navigation.navigate("Home");
     }
   };
-  const cambiarPagina = () =>{
+
+  const cambiarPagina = () => {
     navigation.navigate("Login");
-  }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>{tituloForm}</Text>
@@ -136,8 +125,8 @@ const Formulario = () => {
           <View key={pregunta.Id} style={styles.preguntaContainer}>
             <Text style={styles.clasePregunta}>{pregunta.Texto}</Text>
             <View style={styles.claseBotonesContainer}>
-              <Button onPress={() => onPressSi(pregunta.Id, pregunta.Texto)} title="Si" />
-              <Button onPress={() => onPressNo(pregunta.Id, pregunta.Texto)} title="No" />
+              <Button onPress={() => onPressSi(pregunta.Id, pregunta.Texto, pregunta.IdForm)} title="Si" />
+              <Button onPress={() => onPressNo(pregunta.Id, pregunta.Texto, pregunta.IdForm)} title="No" />
             </View>
             {segundaPregunta[pregunta.Id] && (
               <View style={styles.inputContainer}>
