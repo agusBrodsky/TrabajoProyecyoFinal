afd1dc25fcca28f6eb238a82704b120a63679e22
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-09 12:11:37 -0300

    perrrooo

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,24 +1,168 @@
 import React from 'react';
-import { Text, TouchableNativeFeedback, View,Alert,StyleSheet } from 'react-native';
-import NavBar from '../components/NavBar'; 
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Svg, { Circle } from 'react-native-svg'; // Importa Circle de react-native-svg
+import { useNavigation } from '@react-navigation/native';
 
-//const Home = ({route,navigation}) =>{
-const Home = () =>{
+const NavBar = ({ title }) => {
+  const navigation = useNavigation();
 
-  //const { user } = route.params;
-    
-    return(
-        <View style={styles.container}>
-        <NavBar textoInicio='Home'></NavBar>
-        <Text>No empezado!</Text>
+  const handleFormularioPress = () => {
+    // Navegar a la pantalla "Formulario"
+    navigation.navigate('Formulario');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.navBar}>
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.userIconContainer}>
+          <TouchableOpacity onPress={() => handleProfilePress()}>
+            <Icon name="user" size={30} color="black" />
+          </TouchableOpacity>
         </View>
+      </View>
+      <Text style={styles.reminderText}>Recuerda completar el diario del día</Text>
+      <TouchableOpacity style={styles.button} onPress={handleFormularioPress}>
+        <Text style={styles.buttonText}>Diario del día</Text>
+      </TouchableOpacity>
+      <Text style={styles.recordatorio}>Recuerda tomar tu medicación en</Text>
+      
+      {/* Círculo izquierdo */}
+      <View style={[styles.circleContainer, styles.circleLeft]}>
+        <Svg height="150" width="150">
+          <Circle cx="75" cy="75" r="60" fill="#03C4D0" stroke="#0186A0" strokeWidth="2" />
+        </Svg>
+        <View style={styles.textContainer}>
+          <Text style={styles.timerText}>12</Text>
+          <Text style={styles.timerLabel}>Hora/s</Text>
+        </View>
+      </View>
+
+      {/* Círculo derecho */}
+      <View style={[styles.circleContainer, styles.circleRight]}>
+        <Svg height="150" width="150">
+          <Circle cx="75" cy="75" r="60" fill="#03C4D0" stroke="#0186A0" strokeWidth="2" />
+        </Svg>
+        <View style={styles.textContainer}>
+          <Text style={styles.timerText}>30</Text>
+          <Text style={styles.timerLabel}>Minuto/s</Text>
+        </View>
+      </View>
+
+      {/* Línea divisoria */}
+      <View style={styles.divider} />
+<View style={styles.fullWidthDivider} >
+
+    </View>
+    </View>
+    
     );
-}
+};
+
+const handleProfilePress = () => {
+  // Aquí puedes navegar a la pantalla de perfil de la cuenta
+  // utilizando el sistema de navegación de tu aplicación
+  // por ejemplo, navigation.navigate('Profile');
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        textAlign:'center'
-    }
-})
-export default Home
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  navBar: {
+    backgroundColor: "#03C4D0",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    height: 100,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  userIconContainer: {
+    position: 'absolute',
+    bottom: 10,
+    right: 16,
+  },
+  reminderText: {
+    fontSize: 22,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: 45,
+  },
+  button: {
+    backgroundColor: "#03C4D0",
+    borderRadius: 50,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    alignSelf: 'center',
+    marginTop: 30,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  recordatorio: {
+    fontSize: 22,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: 30,
+  },
+  circleContainer: {
+    alignItems: 'center',
+  },
+  circleLeft: {
+    position: 'absolute',
+    top: 320, // Ajusta la posición vertical del círculo izquierdo
+    left: 100, // Ajusta la posición horizontal del círculo izquierdo
+  },
+  circleRight: {
+    position: 'absolute',
+    top: 395, // Ajusta la posición vertical del círculo derecho
+    left: 170, // Ajusta la posición horizontal del círculo derecho
+  },
+  textContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 35, // Ajusta la posición vertical del texto
+  },
+  timerText: {
+    position: 'absolute',
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    top: 5
+  },
+  timerLabel: {
+    position: 'absolute',
+    fontSize: 16,
+    color: 'white',
+    top: 55
+  },
+  divider: {
+    height: 2,
+    backgroundColor: "#03C4D0",
+    marginHorizontal: 16,
+    top: 250 // Ajusta la posición vertical de la línea divisoria
+  },
+  fullWidthDivider: {
+    height: 2,
+    backgroundColor: "gray", // Color gris
+    marginHorizontal: 0, // Margen horizontal de 0 para ocupar todo el ancho
+    top: 280, // Ajusta la posición vertical de la línea divisoria inferior
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 }, // Sombra hacia abajo
+    shadowOpacity: 0.2, // Opacidad de la sombra
+    shadowRadius: 3, // Radio de la sombra
+  },
+});
+
+export default NavBar;
